b306c38f7dda34ae33cad7bb79e50760bd0ceed5
lvfe <lvfe@tcd.ie>
2019-04-08 18:59:53 +0100

    update key file

--- src/main/java/com/example/demo/HttpClient3.java
+++ src/main/java/com/example/demo/HttpClient3.java
@@ -1,28 +1,28 @@
 package com.example.demo;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStream;
+//import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+//import org.apache.commons.httpclient.HttpClient;
+//import org.apache.commons.httpclient.HttpStatus;
+//import org.apache.commons.httpclient.NameValuePair;
+//import org.apache.commons.httpclient.methods.GetMethod;
+//import org.apache.commons.httpclient.methods.PostMethod;
+//import org.apache.commons.httpclient.params.HttpMethodParams;
 /**
  * Created by lenovo on 08/04/2019.
  */
 public class HttpClient3 {
     private static final int COLLECTOR_TIMEOUT = 60 * 1000;
     private static final int READ_TIMEOUT = 60 * 1000;
-    public static String doGet(String url) {
-        InputStream is = null;
-        BufferedReader br = null;
-        String result = null;
-        HttpClient httpClient = new HttpClient();
-        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(COLLECTOR_TIMEOUT);
-        GetMethod getMethod = new GetMethod(url);
-        getMethod.getParams().setParameter(HttpMethodParams.So_TIMEOUT, READ_TIMEOUT);
-        getMethod.getParams().setParamster(HttpMethodParams.RETRY_HANDLER, new DefaultHttp);
-    }
+//    public static String doGet(String url) {
+//        InputStream is = null;
+//        BufferedReader br = null;
+//        String result = null;
+//        HttpClient httpClient = new HttpClient();
+//        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(COLLECTOR_TIMEOUT);
+//        GetMethod getMethod = new GetMethod(url);
+//        getMethod.getParams().setParameter(HttpMethodParams.So_TIMEOUT, READ_TIMEOUT);
+//        getMethod.getParams().setParamster(HttpMethodParams.RETRY_HANDLER, new DefaultHttp);
+//    }
 }
--- src/main/java/com/example/demo/HttpsClient.java
+++ src/main/java/com/example/demo/HttpsClient.java
@@ -1,30 +1,40 @@
 package com.example.demo;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
+import javax.net.ssl.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 /**
  * Created by lenovo on 08/04/2019.
  */
 public class HttpsClient {
-  public String doGet(String url) {
-    SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
-    sslContext.init(null, new TrustManager[]{new MyX509TrustManager()}, new java.security.SecureRandom());
+  public static String doGet(String url) {
+    SSLContext sslContext;
+
     URL serverUrl;
     HttpsURLConnection conn;
     InputStream is;
     BufferedReader bufferedReader;
     try {
+      sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+      sslContext.init(null, new TrustManager[]{new MyX509TrustManager()}, new java.security.SecureRandom());
       serverUrl = new URL(url);
       conn = (HttpsURLConnection) serverUrl.openConnection();
       conn.setSSLSocketFactory(sslContext.getSocketFactory());
+//      conn.setHostnameVerifier(new TrustAny);
+      conn.setHostnameVerifier(new HostnameVerifier() {
+        @Override
+        public boolean verify(String arg0, SSLSession arg1) {
+          return true;
+        }
+      });
       conn.setRequestMethod("GET");
       conn.setRequestProperty("Content-type", "applicatoin/json");
       conn.setInstanceFollowRedirects(false);
@@ -34,7 +44,7 @@ public class HttpsClient {
       InputStreamReader isr = new InputStreamReader(is, "UTF-8");
       bufferedReader = new BufferedReader(isr);
       String temp = null;
-      while((temp = bufferedReader.readLine())!=null) {
+      while ((temp = bufferedReader.readLine()) != null) {
         buffer.append(temp);
       }
       String result = buffer.toString();
@@ -43,12 +53,20 @@ public class HttpsClient {
       e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } catch (KeyManagementException e) {
+      e.printStackTrace();
+    } catch (NoSuchProviderException e) {
+      e.printStackTrace();
     }
 
-
-
+  return null;
   }
+
+
   public static void main(String[] args) throws Exception {
-    String data = HttpsClient.goGet();
+    String data = HttpsClient.doGet("https://baidu.com");
+    System.out.println(data);
   }
 }
